d3bfe31107b1d16069ae5b1287e9bd8530cbde45
Verity Scheel <monoidmusician@gmail.com>
2022-12-31 17:05:35 -0600

    Update vite config for local samples

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ const DB = process.env.NODE_ENV === "production"
   ? path.resolve(__dirname, "output-es/Main/")
   : path.resolve(__dirname, "output/Main/");
 
-const ASSETS = path.resolve(__dirname, "assets/");
+const ASSETS = process.env.NODE_ENV === "production" || !(require("fs").existsSync("./samples"))
+  ? path.resolve(__dirname, "assets/")
+  : path.resolve(__dirname, "./");
 
 export default {
   resolve: {
